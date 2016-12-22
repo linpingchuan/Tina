37f918710e72d80499163783bd26681a98e5a066
paul <paul_lin@foxmail.com>
2016-12-23 00:00:25 +0800

    ADD add alloc.cpp

--- Tina/Tina/alloc.cpp
+++ Tina/Tina/alloc.cpp
@@ -1,15 +1,15 @@
 ﻿#include "c.h"
-
+// 小于 avail 都是已经被分配的区域
+// [avail,limit] 这段区间，都是继续可分配空间；
+// next 指向链表的下一块
 struct block
 {
-	// 小于 avail 都是已经被分配的区域
-	// [avail,limit] 这段区间，都是继续可分配空间；
-	// next 指向链表的下一块
 	struct block *next;
-	char *limit; 
+	char *limit;
 	char *avail;
 };
-
+// 联合align给出了宿主机上最小的对齐字节数。
+// align的成员规定了最严格的对齐要求
 union align {
 	long l;
 	char *p;
@@ -22,8 +22,9 @@ union header {
 	union align a;
 };
 #ifdef PURIFY
+// 如果PURIFY已定义，则采用malloc和free方式进行内存分配，这种方式方便查错
 // arena : 分配区
-union header *arena[3]; 
+union header *arena[3];
 
 void *allocate(unsigned long n, unsigned a) {
 	union header *_new = malloc(sizeof *_new + n);
@@ -56,6 +57,31 @@ viod *newarray(unsigned long m, unsigned long n, unsigned a) {
 }
 #else
 static struct block
-first[] = { {NULL},{NULL},{NULL} };
+first[] = { {NULL},{NULL},{NULL} },
 *arena[] = { &first[0],&first[1],&first[2] };
+
+static struct block *freeblocks;
+
+// 空间分配:按照内存边界对其原则确定分配空间大小，
+// 据此增加avail指针值，并返回该指针原来的值。
+// 与malloc类似，allocate必须返回一个对齐后的指针，
+// 指针指向的空间可以存放任何类型的值，
+
+void *allocate(unsigned long n, unsigned a) {
+	struct block *ap;
+
+	ap = arena[a];
+	n = roundup(n, sizeof(union align));
+	// while循环不断执行，
+	// 直到ap所指的块至少有n个字节的可分配空间。
+	// 大多数调用allocate的情况，
+	// 这个块就是由allocate的第二个参数所指的分配区的指向的块
+	while (ap->avail + n > ap->limit) {
+
+		
+	}
+	ap->avail += n;
+	return ap->avail - n;
+
+}
 #endif
--- Tina/Tina/c.h
+++ Tina/Tina/c.h
@@ -6,4 +6,14 @@
 #include <limits.h>
 #include <string.h>
 
+// 宏NEW返回一个指向尚未初始化的空间指针
+#define NEW(p,a) ((p)=allocate(sizeof*(p),(a)))
+
+// 宏NEW0在分配空间后，通过C语言的memset将其初始化为 0
+// memset将其第一个参数作为函数结果返回
+#define NEW0(p,a) memset(NEW(((p),(a)),0,sizeof *(p)))
+
+#undef roundup
+#define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
+
 #define NELEMS(a) ((int)(sizeof(a)/sizeof((a)[0])))
