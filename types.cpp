9f4880ccb3b648528a4ea4c31d4ac574031715e5
Paul <paul_lin@foxmail.com>
2017-01-10 22:43:02 +0800

    ADD add type module

--- types.cpp
+++ types.cpp
@@ -738,9 +738,70 @@ void printdecl(Symbol p, Type ty) {
 	}
 }
 /*
-	TODO
+	typestring返回C的声明，该声明指定ty是标识符id的类型。
+	例如，如果ty是
+	(POINTER (ARRAY 10 (POINTER (CHAR))))
+	且id是"string"，typestring返回"char * (*strings)[10]"
+	lcc的-P选项通过在标准错误输出上打印函数的新风格原型和全局变量，
+	帮助将ANSI之前的代码转换为ANSI C代码。
 */
-char *typestring(Type ty, char *id) {
+char *typestring(Type ty, char *str) {
+	for (; ty; ty = ty->type) {
+		Symbol p;
+		switch (ty->op) {
+		case CONST+VOLATILE:case CONST:case VOLATILE:
+			if (isptr(ty->type))
+				str = stringf("%k %s", ty->op, str);
+			else
+				return stringf("%k %s", ty->op, typestring(ty->type, str));
+			break;
+		case STRUCT:case UNION:case ENUM:
+			assert(ty->u.sym);
+			if ((p = findtype(ty)) != NULL)
+				return *str ? stringf("%s %s", p->name, str) : p->name;
+			if (*ty->u.sym->name >= '1'&&*ty->u.sym->name <= '9')
+				warning("unnamed %k in prototype\n", ty->op);
+			if (*str)
+				return stringf("%k %s %s", ty->op, ty->u.sym->name, str);
+			else
+				return stringf("%k %s", ty->op, ty->u.sym->name);
+		case VOID:case FLOAT:case INT:case UNSIGNED:
+			return *str ? stringf("%s %s", ty->u.sym->name, str) : ty->u.sym->name;
+		case POINTER:
+			if (!ischar(ty->type) && (p = findtype(ty)) != NULL)
+				return *str ? stringf("%s %s", p->name, str) : p->name;
+			str = stringf(isarray(ty->type) || isfunc(ty->type) ? "(*%s)" : "*%s", str);
+			break;
+		case FUNCTION:
+			if ((p = findtype(ty)) != NULL)
+				return *str ? stringf("%s %s", p->name, str) : p->name;
+			if (ty->u.f.proto == 0)
+				str = stringf("%s()", str);
+			else if (ty->u.f.proto[0]) {
+				int i;
+				str = stringf("%s(%s", str, typestring(ty->u.f.proto[0], ""));
+				for (i = 1; ty->u.f.proto[i]; i++)
+					if (ty->u.f.proto[i] == voidtype)
+						str = stringf("%s, ....", str);
+					else
+						str = stringf("%s, %s", str, typestring(ty->u.f.proto[i], ""));
+				str = stringf("%s)", str);
+			}
+			else
+				str = stringf("%s(void)", str);
+			break;
+		case ARRAY:
+			if ((p = findtype(ty)) != NULL)
+				return *str ? stringf("%s %s", p->name, str) : p->name;
+			if (ty->type&&ty->type->size > 0)
+				str = stringf("%s[%d]", str, ty->size / ty->type->size);
+			else
+				str = stringf("%s[]", str);
+			break;
+		default:
+			assert(0);
+		}
+	}
 	assert(0);
 	return 0;
 }
